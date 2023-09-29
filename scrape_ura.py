ce249f8d515652ed218c93e9019022a2aa59aa63
yenchingp <phuayenching@gmail.com>
2023-09-30 01:30:57 +0800

    fix: clean up codes

--- scrape_ura.py
+++ scrape_ura.py
@@ -7,14 +7,9 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 
-# List of condominium names
 condo_names = ['DAHLIA DRIVE']
-
-# Initialize Selenium WebDriver (Chrome)
 driver = webdriver.Chrome()
 
-
-# Function to scrape the data for a single condominium
 def scrape_condo_info(condo_name):
     # Go to the URA website
     driver.get("https://www.ura.gov.sg/maps/?service=MP")
@@ -32,7 +27,7 @@ def scrape_condo_info(condo_name):
     )
     first_address.click()
 
-    # Wait for the 'See Other Services' button to be clickable and click it
+    # wait and click 'See Other Services'
     see_other_services_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, ".us-ip-svcs-l2-other-services"))
     )
@@ -44,43 +39,43 @@ def scrape_condo_info(condo_name):
     )
     explore_dev_site_button.click()
 
-    # Wait for the 'Redevelop Site' tab to be clickable and click it
+    # wait and click 'Redevelop site'
     redevelop_tab = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="us-c-ip"]/div[3]/div/div[2]/div/div[2]/div[1]/div[1]'))
     )
     redevelop_tab.click()
 
-    # Wait for the GPR (Gross Plot Ratio) value to load and print it
+    # GPR
     gpr_value = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="us-c-ip"]/div[3]/div/div[2]/div/div[2]/div[1]/div[5]/div[2]/div[1]/div[2]/div[1]/div[2]'))
     )
     print(f'Gross Plot Ratio for {condo_name}: {gpr_value.text}')
 
-    #building height control restrictions
+    # building height control restrictions
     height_value = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="us-c-ip"]/div[3]/div/div[2]/div/div[2]/div[1]/div[5]/div[2]/div[1]/div[3]/div[1]/div[2]'))
     )
     print(f'Building height control restrictions for {condo_name}: {height_value.text}')
 
-    #maximum dwelling units
+    # maximum dwelling units
     max_dwelling_units = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="us-c-ip"]/div[3]/div/div[2]/div/div[2]/div[1]/div[5]/div[2]/div[1]/div[6]/div[5]/div/div'))
     )
     print(f'Maximum dwelling units for {condo_name}: {max_dwelling_units.text}')
 
-    #landscape replacement area
+    # landscape replacement area
     lra = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="us-c-ip"]/div[3]/div/div[2]/div/div[2]/div[1]/div[5]/div[2]/div[1]/div[7]/div[5]/div/div'))
     )
     print(f'Landscape replacement area(LRA) for {condo_name}: {lra.text}')
 
-    # Wait for the 'See more' link to be clickable and click it
+    # wait and click on 'See more'
     see_more_link = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="us-c-ip"]/div[3]/div/div[2]/div[1]/div[2]/div[1]/div[5]/div[2]/div[1]/div[4]/div[2]/div[2]/a'))
     )
     see_more_link.click()
 
-    # Wait for the setback requirements section to load
+    # setback requirements
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="us-svcs-site-dev-pp-sb-rrl-card"]/div/div[1]'))
     )
@@ -108,37 +103,38 @@ def scrape_condo_info(condo_name):
             # Exit the loop if the element isn't found, which likely means we've processed all cards
             break
 
-    #section to take ss:
+    # section to take ss
     # click layers tab
     layers = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '// *[ @ id = "us-map-layers"]'))
     )
     layers.click()
 
+    # wait and locate location pin
     WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".leaflet-marker-icon"))
     )
-    marker_icon = driver.find_elements(By.CSS_SELECTOR, ".leaflet-marker-icon")[0]  # Assume the first marker is the one of interest
+    marker_icon = driver.find_elements(By.CSS_SELECTOR, ".leaflet-marker-icon")[0]
 
-    # Ensure marker is in view
+    # ensure location pin is in view
     driver.execute_script("arguments[0].scrollIntoView();", marker_icon)
 
-    # Zoom in using ActionChains
+    # zoom in to maximum zoon
     actions = ActionChains(driver)
     actions.move_to_element(marker_icon)
-    for _ in range(20):
+    for _ in range(50):
         actions.send_keys(Keys.ADD)
     actions.perform()
 
-    # Re-find the marker to avoid StaleElementReferenceException
+    # relocate location pin avoid StaleElementReferenceException
     marker_icon = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".leaflet-marker-icon.leaflet-zoom-animated"))
     )
 
-    # Ensure marker is in view again
+    # ensure location pin is in view again
     driver.execute_script("arguments[0].scrollIntoView();", marker_icon)
 
-    # Take a screenshot
+    # ss of site
     driver.save_screenshot('screenshot_area.png')
 
     # toggle on the site plan view
@@ -147,14 +143,14 @@ def scrape_condo_info(condo_name):
     )
     site_layout_toggle.click()
 
+    # force sleep so it can apply the site plan view
     time.sleep(10)
 
-    # Take a screenshot
+    # ss of site plan
     driver.save_screenshot('screenshot_site_plan.png')
 
-# Iterate over the condominium names and scrape the info for each
+# iterate over the condominium names
 for condo_name in condo_names:
     scrape_condo_info(condo_name)
 
-# Close the WebDriver
 driver.quit()
